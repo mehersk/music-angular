54c51603dade27aca4f8dacdc4c3e13974afdb79
Meher Supreeth <mehersupreeth@gmail.com>
2020-11-12 14:09:53 +0530

    initial commit

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
  
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/dist/music-angular'));
+app.use(express.static(__dirname + '/dist/challenge77-music-app'));
  
 app.get('/*', function(req,res) {
  
-res.sendFile(path.join(__dirname+'/dist/music-angular/index.html'));
+res.sendFile(path.join(__dirname+'/dist/challenge77-music-app/index.html'));
 });
  
 // Start the app by listening on the default Heroku port
